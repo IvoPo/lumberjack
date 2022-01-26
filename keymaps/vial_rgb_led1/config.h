9375568f180882ab48ca0316e317ebd10edec620
IvoPo <selfkiller@hotmail.com>
2022-01-26 10:14:17 +0100

    init

---
+++ keymaps/vial_rgb_led1/config.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x25, 0x82, 0xE8, 0x6B, 0x00, 0x2D, 0xE7, 0x55}
+
+
+#define VIAL_UNLOCK_COMBO_ROWS { 3, 3 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 5 }
+
+#define RGB_DI_PIN C5
+#undef RGBLED_NUM
+#define RGBLED_NUM 14
